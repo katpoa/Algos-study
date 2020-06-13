ee4cd37fd777fd254ccd2751751866686200aaef
katpoa <57083893+katpoa@users.noreply.github.com>
2020-06-12 23:37:06 -0700

    Create listLinkedListSupportedOperations.js

---
+++ listLinkedListSupportedOperations.js
@@ -0,0 +1,24 @@
+/*
+***List - ordered sequence of elements [1 2 3 4 5]
+Supported operations: *Only add/delete from end
+Lookup by location/index is O(1)
+Modify element at index O(1)
+Delete from end O(1)
+Add to end O(1)
+
+Can add behavior to a list:
+Add to beginning, random deletion- O(n) - essentially have to modify each element after and then delete from end
+Does it contain something? O(n) *for set, O(1)
+.indexOf() - O(n)
+Filing cabinets, looking by label vs finding 
+
+***Linked List - some information with instructions to the next in the sequence, essentially ordered sequence as well, less efficient, have to follow the directions to the next one
+Random addition much easier just manipulate the links and the element
+Supported operations:  Follow chain O(1)
+Manipulate chain (Delete/insert at any spot) O(1)
+
+Can add behavior to a list:
+Does it contain something? O(n)
+Lookup at specific index O(n)
+Go to a spot O(n)
+*/
